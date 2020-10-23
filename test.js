8293b4f19ca47e105d4481e37e57da029307bb09
deonet <72336616+deonet@users.noreply.github.com>
2020-10-24 00:44:09 +0700

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,23 @@ const Uniprice = require('./index')
 
 // SPANK token address
 let spankExchangeAddress = '0x4e395304655f0796bc3bc63709db72173b9ddf98';
+//let spankExchangeAddress = '';
 
 async function main() {
+
   const uniprice = Uniprice()
-  const spankSwap = uniprice.setExchange('SPANK', '0x4e395304655f0796bc3bc63709db72173b9ddf98')
+  //const spankSwap = uniprice.setExchange('SPANK', '0x4e395304655f0796bc3bc63709db72173b9ddf98')
+  const spankSwap = uniprice.setExchange('a1',
+    '0x4e395304655f0796bc3bc63709db72173b9ddf98')
+
+
+  console.log('ContractCreator:', await uniprice.factoryAddress );
 
   console.log(await spankSwap.getPrice())
-  console.log(await uniprice.getPrice('SPANK'))
+  console.log(await uniprice.getPrice('a1'))
   // console.log(uniprice.exchanges)
 
-  uniprice.dropExchange('SPANK')
+  uniprice.dropExchange('a1')
   // console.log(uniprice.exchanges)
 }
 
